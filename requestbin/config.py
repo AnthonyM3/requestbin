791994432be2260693e3ae0dacc153b526eab312
AnthonyM3 <46538669+AnthonyM3@users.noreply.github.com>
2019-01-10 12:07:42 -0600

    Updated ROOT URL

--- requestbin/config.py
+++ requestbin/config.py
@@ -30,7 +30,7 @@ BUGSNAG_KEY = ""
 
 if REALM == 'prod':
     DEBUG = False
-    ROOT_URL = "http://requestb.in"
+    ROOT_URL = "https://udn-events.herokuapp.com/"
 
     FLASK_SESSION_SECRET_KEY = os.environ.get("SESSION_SECRET_KEY", FLASK_SESSION_SECRET_KEY)
 
